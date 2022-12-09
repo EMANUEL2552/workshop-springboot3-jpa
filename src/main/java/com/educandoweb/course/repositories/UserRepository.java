d0d90489e7bec3677a179dee0133235f0d520130
EMANUEL2552 <emanuel.fernandesdasilva@gmail.com>
2022-12-09 18:40:26 -0300

    JPA repository dependency injection,database seeding

---
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.educandoweb.course.repositories;
+
+import java.util.List;
+import java.util.Arrays;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	
+	
+	
+
+}
